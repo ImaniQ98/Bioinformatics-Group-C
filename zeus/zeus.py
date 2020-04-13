f2c4d2f276cdd3f947b37ec8a0ed4aa1d5aca4cf
Jearce <jessiearce42@gmail.com>
2020-04-13 12:21:08 -0500

    Added command line interface with argparse module.

--- zeus/zeus.py
+++ zeus/zeus.py
@@ -5,6 +5,7 @@ Zeus is a command line program that takes forward and reverse reads in fastq for
 Author: Jessie Arce
 """
 import sys
+import argparse
 import subprocess
 import os
 
@@ -15,25 +16,42 @@ RESULTS_DIR = "zeus_ouput/"
 ANNOTATE_DIR = RESULTS_DIR+"annos/"
 
 def main():
-        num_args = len(sys.argv[1:])
-        if num_args == 4:
-            forward_reads,reverse_reads,dbname,dbseqs = sys.argv[1:]
-        else:
-            print("useage: zeus.py <forward_reads> <reverse_reads> <dbname> <dbseqs>")
-            exit()
+    parser = argparse.ArgumentParser(description="Assembles and annotates genome given a set of proteins.")
+    parser.add_argument("F",
+                        type=str,
+                        metavar="<forward reads>",
+                        help="Forward reads in FastQ format."
+                        )
+    parser.add_argument("R",
+                        type=str,
+                        metavar="<reverse reads>",
+                        help="Reverse reads in FastQ format."
+                        )
+    parser.add_argument("dbname",
+                        type=str,
+                        metavar="<database name>",
+                        help="Name for the blast databse.")
 
-        assemble.assemble_genome(
-            forward_reads,
-            reverse_reads,
-            RESULTS_DIR
-        )
+    parser.add_argument("dbseqs",
+                        type=str,
+                        metavar="<database sequences>",
+                        help="Set of reference proteins.")
 
-        annotate.annotate_proteins(
-            RESULTS_DIR+'spades_output/contigs.fasta',
-            ANNOTATE_DIR,
-            dbname,
-            dbseqs
-        )
+    args = parser.parse_args()
+
+
+    assemble.assemble_genome(
+        args.F,
+        args.R,
+        RESULTS_DIR
+    )
+
+    annotate.annotate_proteins(
+        RESULTS_DIR+'spades_output/contigs.fasta',
+        ANNOTATE_DIR,
+        args.dbname,
+        args.dbseqs
+    )
 
 if __name__ == "__main__":
     main()
